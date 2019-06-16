8380b9cda373e2e44f49b1ec103ea89ba58519cd
bixnel <hi@bixnel.ru>
2019-06-16 13:19:00 +0300

    CORS policy :name_badge:

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,40 @@
 import React, { Component } from "react";
+import ReactTooltip from 'react-tooltip'
 import '../styles/App.css';
+
+
+
+class Latency extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {latency: 0};
+  }
+
+  timer() {
+    const start = new Date().getTime();
+    fetch("https://ya.ru", {mode: 'cors'})
+      .then(
+        (response) => {
+          if (response.status == 200) {
+            const end = new Date().getTime();
+            this.setState({latency: end - start});
+          }
+        }
+      )
+    console.log(end - start);
+  }
+
+  render() {
+    return (
+        <span className="ping good" data-tip="Latency: ${this.state.latency}ms"><ReactTooltip /></span>
+    );
+  }
+
+  componentDidMount() {
+    setInterval(this.timer, 1000);
+  }
+}
+
 
 const ChatHeader = (props) => (
   <header className="header">
@@ -7,61 +42,88 @@ const ChatHeader = (props) => (
       <img src="https://images.unsplash.com/photo-1517849845537-4d257902454a?w=60" alt="logo" />
     </div>
     <div className="info">
-      <h1 className="chatname">{props.chatname} <span className="ping"></span></h1>
+      <h1 className="chatname">{props.chatname} <Latency /></h1>
       <p className="users">{props.online} пользователь онлайн</p>
     </div>
   </header>
 )
 
-const ChatBox = (props) => (
-  <section className="chatbox">
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-    <div className="msg from-them">Лол привет</div>
-    <div className="msg from-me">Добрый день</div>
-  </section>
-)
 
-const SendMessage = () => (
-  <footer className="sendmessage">
-    <textarea placeholder="Сообщение"></textarea>
-    <svg width="20" height="20" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z"/></svg>
-  </footer>
-)
+class ChatBox extends Component {
+  render() {
+    return (
+      <section className="chatbox">
+        <div className="msg from-them">Лол привет ваопварполвропрвлапролварпврпловрапловралпорвалпрвалпорлваопрлваорплваорпвлаопрлваопрлваопрлваорпл</div>
+        <div className="msg from-me">Лол привет ваопварполвропрвлапролварпврпловрапловралпорвалпрвалпорлваопрлваорплваорпвлаопрлваопрлваопрлваорпл</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+        <div className="msg from-them">Лол привет</div>
+        <div className="msg from-me">Добрый день</div>
+      </section>
+    );
+  }
+}
+
+
+class SendMessage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {inProcess: false};
+    this.send = this.send.bind(this);
+  }
+
+  send() {
+    console.log(this.state.inProcess);
+    this.setState({inProcess: true});
+    console.log(this.state.inProcess);
+  }
+
+  render() {
+    return (
+      <footer className="sendmessage">
+        <textarea placeholder="Сообщение"></textarea>
+        <div className="send">
+          <svg onClick={this.send} width="25" height="25" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z"/></svg>
+          <p className="counter">{this.props.counter}</p>
+        </div>
+      </footer>
+    );
+  }
+}
+
 
 const Chat = () => (
   <main className="chat">
     <ChatHeader chatname="Chatname" online="10" />
-
-    <SendMessage />
+    <ChatBox />
+    <SendMessage counter="130"/>
   </main>
 )
+
 
 class App extends Component {
   render() {
@@ -70,5 +132,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const autoprefixer = require('autoprefixer');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+
 module.exports = {
   entry: "./src/main.js",
   output: {
