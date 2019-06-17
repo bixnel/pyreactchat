8ae43e7c5b6736afb045ca77e97afa56f20cddb9
bixnel <hi@bixnel.ru>
2019-06-17 12:41:54 +0300

    Add redirect to GitHub Pages

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
+import os
 import json
 from random import randint
 from tornado.ioloop import *
 from tornado.web import *
 from tornado.websocket import *
+
+
+
+class Index(RequestHandler):
+    def get(self):
+        self.redirect('https://bixnel.github.io/pyreactchat/')
 
 
 class Online(RequestHandler):
@@ -17,7 +24,7 @@ class IP(RequestHandler):
     def get(self):
         self.set_header('Access-Control-Allow-Origin', '*')
         self.set_header('Content-Type', 'application/json')
-        self.write(self.request.remote_ip)
+        self.write(self.request.headers.get('X-Forwarded-For'))
 
 
 class Chat(WebSocketHandler):
@@ -25,39 +32,35 @@ class Chat(WebSocketHandler):
         return True
 
     def open(self):
-        print("WebSocket opened", self.request.remote_ip)
-        users[str(self.request.remote_ip)] = self
-        print(users)
+        users[str(self.request.headers.get('X-Forwarded-For'))] = self
 
     def on_message(self, message):
         message = json.loads(message)
-        print('MESSAGE: ', message)
         if message['action'] == 'get_messages':
             data = {'action': 'messages',
                     'data': messages}
             self.write_message(json.dumps(data))
         elif message['action'] == 'send_message':
-            messages.append([self.request.remote_ip, message['data'], randint(-2147483648, 2147483647)])
+            messages.append([self.request.headers.get('X-Forwarded-For'), message['data'], randint(-2147483648, 2147483647)])
             data = {'action': 'messages',
                     'data': messages}
             for user in users.keys():
                 users[user].write_message(json.dumps(data))
 
     def on_close(self):
-        print("WebSocket closed")
-        del users[str(self.request.remote_ip)]
-        print(users)
+        del users[str(self.request.headers.get('X-Forwarded-For'))]
 
 
 application = Application([
+    ('/?$', Index),
     ('/api/online/?$', Online),
     ('/api/ip/?$', IP),
-    ('/ws/?$', Chat),
 ])
+('/ws/?$', Chat),
 
 
 if __name__ == '__main__':
     users = {}
     messages = []
-    application.listen(3333)
+    application.listen(os.getenv('PORT', 3333), address = '0.0.0.0')
     IOLoop.current().start()
